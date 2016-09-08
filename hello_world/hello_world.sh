0b068c74967d0b5eaf5ba54104161e4d2342892c
Eilif Muller <eilif.mueller@epfl.ch>
2016-09-08 06:47:14 +0000

    stuff

--- hello_world/hello_world.sh
+++ hello_world/hello_world.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -l 
 #$ -V -N hello_world.py -cwd -o output.$JOB_NAME.$JOB_ID
 
 # Run with: qsub -pe mpislots N hello_world.sh
 
+sleep 10
 module load apps/anaconda3/2.5.0/bin
 module load mpi/openmpi/1.8.5/gcc-4.8.5
 
